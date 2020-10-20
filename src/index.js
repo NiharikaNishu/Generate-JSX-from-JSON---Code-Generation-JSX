dfca3bd25c13e5bbf26a7c30dc26f34d6fdc2ca9
Niharika Nishu <46600925+NiharikaNishu@users.noreply.github.com>
2020-10-20 13:21:24 +0530

    Code changed

--- src/index.js
+++ src/index.js
@@ -1,48 +1,50 @@
 function toTitleCase(str) {
-    return str.replace(
-        /\w\S+/g,
-        function(txt){
-            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-        }
-        ).replace(" ","");
+  return str
+    .replace(/\w\S+/g, function (txt) {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    })
+    .replace(" ", "");
 }
 
-function toCamelCase(str){
-    str = str.replace("-"," ")
-    return str
-          .replace(/\s(.)/g,function($1) { return $1.toUpperCase(); }) 
-          .replce(/\s/g, '')
-          .replace(/^(.)/, function($1) { return $1.toLowerCase(); })
+function toCamelCase(str) {
+  str = str.replace("-", " ");
+  return str
+    .replace(/\s(.)/g, function ($1) {
+      return $1.toUpperCase();
+    })
+    .replce(/\s/g, "")
+    .replace(/^(.)/, function ($1) {
+      return $1.toLowerCase();
+    });
 }
 
 function recur(obj) {
-    let ret_str= "<"
-    ret_str += toTitleCase(obj.name)+ " "
-    if (obj.style != undefined && Object.keys(obj.style).length > 0){
-        var stypeKeys = Object.keys(obj.syle)
-        ret_str += "style={{"
-        for(var i = 0; i < stypeKeys.length; i++){
-        ret_str += toCamelCase(stypeKeys[i]) + ':"' + obj.style[stypeKeys[i]]+ '",'
-        }
-        ret_str = ret_str.substr(0, ret_str.length - 1) + "}}"
+  let ret_str = "<";
+  ret_str += toTitleCase(obj.name) + " ";
+  if (obj.style != undefined && Object.keys(obj.style).length > 0) {
+    var stypeKeys = Object.keys(obj.syle);
+    ret_str += "style={{";
+    for (var i = 0; i < stypeKeys.length; i++) {
+      ret_str +=
+        toCamelCase(stypeKeys[i]) + ':"' + obj.style[stypeKeys[i]] + '",';
     }
+    ret_str = ret_str.substr(0, ret_str.length - 1) + "}}";
+  }
 
-    if(obj.children !== undefined && obj.children.length > 0){
-        ret_str += ">\n";
-        for(var i = 0; i < obj.children.length; i++){
-            ret_str += recur(obj.children[i])
-        }
-        ret_str += "</" + toTitleCase(obj.name) +">"
+  if (obj.children !== undefined && obj.children.length > 0) {
+    ret_str += ">\n";
+    for (var i = 0; i < obj.children.length; i++) {
+      ret_str += recur(obj.children[i]);
     }
-    else {
-        ret_str += '/>'
-    }
-    return ret_str
+    ret_str += "</" + toTitleCase(obj.name) + ">";
+  } else {
+    ret_str += "/>";
+  }
+  return ret_str;
 }
 
 function generateCodeFromObject(obj) {
-    return recur(obj)
-  }
-  
-  module.exports = generateCodeFromObject;
-  
+  return recur(obj);
+}
+
+module.exports = generateCodeFromObject;
